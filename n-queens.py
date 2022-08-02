274c00b8670c0406d82203709baf21a45365443c
Mikey Mike <tauon888@gmail.com>
2022-08-02 18:16:13 +1000

    Remove 2D array and make board a 1D array of queen columns for each row.

--- n-queens.py
+++ n-queens.py
@@ -12,36 +12,42 @@
 import time
 import argparse
 
-n = 5
 solutions = 0
-grid = []
+board = []
 cols_used = set()
 up_diags_used = set() # Used to track placed queen's (row + col) diagnonals.
 down_diags_used = set() # Used to track placed queen's (row - col) diagnonals.
 
-def print_grid(s, n):
+def print_board(s, n):
     print("Solution:", s)
-    for row in grid:
+    for queen in board:
         print("+---" * n + "+")
-        for cell in row:
-            print('|', cell, end=" ")
+        for col in range(0, n):
+            if col == queen:
+                print('|', 'Q', end=" ")
+            else:
+                print('|', ' ', end=" ")
         print('|')
     print("+---" * n + "+")
-    return
+    print()
 
 
 def check_unique(position, n):
     return True
 
-def solve(row, n):
-    global grid
+
+def solve(row, n, unique):
+    global board
     global solutions
 
     if row == n:
         position = 0
-        if check_unique(position, n):
+        if unique and check_unique(position, n):
             solutions += 1
-            print_grid(solutions, n)
+            print_board(solutions, n)
+        else:
+            solutions += 1
+            print_board(solutions, n)
         return
 
     for col in range(0, n):
@@ -51,23 +57,20 @@ def solve(row, n):
         cols_used.add(col)
         up_diags_used.add(row + col)
         down_diags_used.add(row - col)
-        grid[row][col] = 'Q'
+        board[row] = col
 
         # Solve the next row by calling ourself recursively.
-        solve(row + 1, n)
+        solve(row + 1, n, unique)
 
         # Now backtrack to find more solutions.
         cols_used.remove(col)
         up_diags_used.remove(row + col)
         down_diags_used.remove(row - col)
-        grid[row][col] = ' '
-
-    return
+        board[row] = 0
 
 
 def main():
-    global grid
-    global n
+    global board
 
     # Check for any command line args.
     parser = argparse.ArgumentParser(description='This program computes the solutions to the n-queens problem.  That is, how to place n-queens on a chessboard, so they cannot take each other.')
@@ -78,20 +81,23 @@ def main():
     #print(vars(args))
 
     n = args.size
-    all_sols = args.all
+    unique = not args.all
+    version = 'V1.0'
+    print('N-Queens Solver {}'.format(version))
 
     # Show the board size that will be used.
-    if all_sols:
-        print(f'Finding all solutions using a {n} x {n} board...\n')
+    if unique:
+        print(f'Finding unique solutions using a {n}x{n} board...\n')
     else:
-        print(f'Finding unique solutions using a {n} x {n} board...\n')
+        print(f'Finding all (non-unique) solutions using a {n}x{n} board...\n')
 
     # Define an empty grid of the given size.
-    grid = [[' '] * n for i in range(0, n)]
+    #grid = [[' '] * n for i in range(0, n)]
+    board = [0 for i in range(0, n)]
 
     # Take note of the start time and begin.
     start_time = time.time()
-    solve(0, n)
+    solve(0, n, unique)
 
     # Print the run-time.
     seconds = time.time() - start_time
